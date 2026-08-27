328fdc326acc5c40fb49202ee26df38b74840b19
DeLorean Wiggins <321556840+wigginsdga-cell@users.noreply.github.com>
2026-08-27 07:59:42 -0400

    title change

--- components/navbar-component.js
+++ components/navbar-component.js
@@ -2,7 +2,7 @@ export default {
   name: 'navbar-component',
   template: /* html */ `
     <nav class="navbar sticky-top bg-white border-bottom px-3">
-      <span class="navbar-brand mb-0 h1"><i class="bi bi-bootstrap-fill me-2"></i>Web App Starter</span>
+      <span class="navbar-brand mb-0 h1"><i class="bi bi-bootstrap-fill me-2"></i>C450 Web App</span>
 
       <div class="ms-auto d-flex gap-2">
         <router-link class="btn btn-outline-primary btn-sm" to="/">
